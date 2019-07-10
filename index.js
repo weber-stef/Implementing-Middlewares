fec7dc95c1f88534e1d93e39a0c85af6be9b7936
Stefan Wurck <vitatech@gmx.de>
2019-07-10 15:11:08 +0200

    task 1.1 done

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
-const app = express()
+const app = express();
+const port = 3000;
+const router = express.Router();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+router.use('/1', function (req, res, next) {
+  console.log('Requested Route:', req.originalUrl, 'HTTP-Method used:', req.method);
+  next();
+}, function (req, res, next) {
+  res.send("1. task accomplished, as far as I can tell..");
 
-app.listen(3000)
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
+// mount the router on the app
+app.use('/', router);
